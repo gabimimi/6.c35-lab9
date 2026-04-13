4ad0ee5723ee2439b10b93dd2f049bfe5d83d649
Gabriela Miranda <gmmirand@mit.edu>
2026-04-13 14:40:30 -0400

    flatten once more

--- lab_9_skeleton/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<h1>My Map</h1>
-<p>
-	An interactive web map for exploring bike-share and geographic data, built with SvelteKit and
-	MapLibre GL.
-</p>
-
-<style>
-	@import url("$lib/global.css");
-</style>
