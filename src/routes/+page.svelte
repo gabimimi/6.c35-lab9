2eac923115a47ccb23a056d8893d5f113c6bda46
Gabriela Miranda <gmmirand@mit.edu>
2026-04-13 14:36:03 -0400

    Move lab_9_skeleton contents to repo root

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>My Map</h1>
+<p>
+	An interactive web map for exploring bike-share and geographic data, built with SvelteKit and
+	MapLibre GL.
+</p>
+
+<style>
+	@import url("$lib/global.css");
+</style>
